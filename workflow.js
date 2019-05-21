1a3946139bf085510a3d6219f7606d6173caca71
cuttinger <iamapc@163.com>
2019-05-21 01:47:28 -0700

    Add files via upload

---
+++ workflow.js
@@ -0,0 +1,16 @@
+﻿
+setInterval(function () {
+    if (location.href.indexOf("youtube") > 0) {
+        //console.log("checking...");
+        try {
+            if ($(".ytp-ad-skip-button")) {
+                //console.log("find button");
+                $(".ytp-ad-skip-button").click();
+                //console.log("click button");
+            }
+        }
+        catch (e) {
+            //console.log(e.message);
+        }
+    }
+}, 1000);
